14aa68810b781bc727dc15f600af839e14d55f20
KapilRaghuwanshi <Kapil.Raghuwanshi@cognizant.com>
2018-11-19 20:17:32 +0530

    Added markers methods

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, ViewChild, NgZone  } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ElementRef, ViewChild, NgZone } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
 import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 import 'rxjs/add/operator/filter';
 
@@ -25,13 +25,16 @@ export class HomePage {
     timeout: 1000,
     maximumAge: 0
   };
+  markers = [];
 
-  constructor(public navCtrl: NavController, private geolocation: Geolocation, private zone : NgZone) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation,
+    private zone: NgZone, private platform: Platform) 
+    {
+    platform.ready().then(() => {
+      this.initMap();
+    });
   }
 
-  ionViewDidLoad() {
-    this.initMap();
-  }
 
   initMap() {
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
@@ -39,6 +42,24 @@ export class HomePage {
       center: { lat: 17.38, lng: 78.48 }
     });
     this.directionsDisplay.setMap(this.map);
+
+    this.geolocation.getCurrentPosition(this.options)
+    .then((resp) => {
+      let mylocation = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 15,
+        center: mylocation
+      });
+    });
+
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.deleteMarkers();
+      let updatelocation = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
+      let image = 'assets/icon/Byk.png';
+      this.addMarker(updatelocation, image);
+      this.setMapOnAll(this.map);
+    });
   }
 
   fetchCurrentLocation() {
@@ -105,8 +126,8 @@ export class HomePage {
         this.map.setZoom(9);
 
         this.zone.run(() => {
-           this.moveLatLang = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-         });
+          this.moveLatLang = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        });
 
         //if (this.startMarker) {
         // Marker already created - Move it
@@ -118,24 +139,40 @@ export class HomePage {
           animation: google.maps.Animation.DROP
         });
 
-        this.startMarker.setMap(this.map);
-        //this.startMarker.setPosition(this.startLatLang);
-        //}
-        // else {
-        //   // Marker does not exist - Create it
-        //   this.startMarker = new google.maps.Marker({
-        //     position: this.startLatLang,
-        //     map: this.map,
-        //     animation: google.maps.Animation.DROP,
-        //     title: "Moving towards destination",
-        //     icon: { url: "assets/icon/PickUp.png", scaledSize: new google.maps.Size(25, 40) },
-        //   });
-        // }
+        //this.startMarker.setMap(this.map);
+        this.startMarker.setPosition(this.moveLatLang);
+
       });
     //To stop notifications
     //watch.unsubscribe();
   }
 
+  addMarker(location, image) {
+    let marker = new google.maps.Marker({
+      position: location,
+      title: "yourTitle",
+      map: this.map,
+      icon: { url: image, scaledSize: new google.maps.Size(25, 40) },
+      animation: google.maps.Animation.DROP
+    });
+    this.markers.push(marker);
+    console.log(this.markers);
+  }
+
+  setMapOnAll(map) {
+    for (var i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+
+  deleteMarkers() {
+    this.clearMarkers();
+    this.markers = [];
+  }
+
+  clearMarkers() {
+    this.setMapOnAll(null);
+  }  
 
   // drop() {
   //   this.clearMarkers();
@@ -148,7 +185,9 @@ export class HomePage {
   //   window.setTimeout(function () {
   //     this.markers.push(new google.maps.Marker({
   //       position: position,
+  //       title: "Moving from PickUp",
   //       map: this.map,
+  //       icon: { url: "assets/icon/Byk.png", scaledSize: new google.maps.Size(40, 40) },
   //       animation: google.maps.Animation.DROP
   //     }));
   //   }, timeout);
